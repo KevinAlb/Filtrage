32ae97995e8263b445e459b6978f7d1bed2729b2
Kevin <kevin.albertus@hotmail.fr>
2016-10-05 14:19:31 +0200

    Creation d'un bruit gaussian

--- gaussian.m
+++ gaussian.m
@@ -4,7 +4,7 @@ function [out] = gaussian(in,mean,variance)
 out = zeros(row,column);
 for i = 1:row
   for j = 1:column
-    bruit = round(mean - 1 + (256 * variance * randn(1)));
+    bruit = mean - 1 + (256 * variance * randn(1));
     out(i,j) = in(i,j) + bruit;
     if out(i,j) < 0
       out(i,j) = 0;
