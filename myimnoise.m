2f384dfc4966bfea080d081e638a60cda0a3b46e
Kevin <kevin.albertus@hotmail.fr>
2016-10-05 15:53:21 +0200

    Create gaussian noise and salt and pepper noise

---
+++ myimnoise.m
@@ -0,0 +1,20 @@
+function out = myimnoise(in, type, a,b) 
+
+out = im2double(in);
+switch (type)
+
+  case "salt & pepper"
+    bruit = rand(size(in));
+    out(bruit <= a/2) = 0;
+    out(bruit >= 1 - a/2) = 1;
+  
+  
+  case "gaussian"
+    bruit =  (a + randn(size(out)) * sqrt(b))
+    out = out .+ bruit;
+    out(out > 1) = 1;
+    out(out < 0) = 0;
+    
+endswitch 
+
+endfunction   
